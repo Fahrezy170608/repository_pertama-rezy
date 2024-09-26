4aaa6ba5cd782d11ed0dc2bb4d431af35a266899
arifalfazhar <rokokksp03@gmail.com>
2024-09-26 12:05:31 +0700

    baru

---
+++ hari_dua/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$luas=7;
+const phi=3.14;
+
+echo $luas;
+echo"<br>";
+echo phi;
+echo "<br>";
+
+$luasPersegiPanjang=67;
+echo $luasPersegiPanjang;echo "<br>";
+
+$luasPersegiPanjang=67;
+$kelilingLingkaran=19.5;
+$nama="Haliq";
+$mati=True;
+
+echo "<br><h4>Tipe Data</h4>";
+var_dump($luasPersegiPanjang);echo "<br>";
+var_dump($kelilingLingkaran);echo "<br>";
+var_dump($nama);echo "<br>";
+var_dump($mati);echo "<br>";
